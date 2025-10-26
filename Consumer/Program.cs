a4f0ca5bea80bfef3b044279044c3d84ccc5e227
makv1975@hotmail.com <makv1975@hotmail.com>
2025-10-26 15:13:59 +0000

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Messaging.ServiceBus;
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the sender used to publish messages to the queue
+ServiceBusSender sender;
+
+// number of messages to be sent to the queue
+const int numOfMessages = 3;
+
+client = new ServiceBusClient(
+    "Endpoint=sb://localhost:5672;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;");
+sender = client.CreateSender("queue.1");
+
+var processor = client.CreateProcessor("queue.1");
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("wait");
+    Console.ReadKey();
+    Console.WriteLine("stopping");
+    await processor.StartProcessingAsync();
+    Console.WriteLine("stopped");
+}
+catch
+{
+
+}
+async Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.ErrorSource);
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    Console.WriteLine(args.Message);
+}
+
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
---
+++ sbapp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Messaging.ServiceBus;
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the sender used to publish messages to the queue
+ServiceBusSender sender;
+
+// number of messages to be sent to the queue
+const int numOfMessages = 3;
+
+client = new ServiceBusClient(
+    "Endpoint=sb://localhost:5672;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;");
+sender = client.CreateSender("queue.1");
+
+// create a batch 
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <= numOfMessages; i++)
+{
+    // try adding a message to the batch
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        // if it is too large for the batch
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    // Use the producer client to send the batch of messages to the Service Bus queue
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
